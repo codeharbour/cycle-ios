da0a6a084fdfd3698933c8448c7a4214834adb42
Pete Nelson <pete.nelson@mac.com>
2014-11-21 16:23:15 +0000

    Initial Commit

---
+++ Brakes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Brakes
+//
+//  Created by Pete Nelson on 21/11/2014.
+//  Copyright (c) 2014 codeharbour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Brakes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Brakes
+//
+//  Created by Pete Nelson on 21/11/2014.
+//  Copyright (c) 2014 codeharbour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
